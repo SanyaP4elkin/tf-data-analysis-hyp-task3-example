1c35d8e354478667e68ce2c38310c5f59388a44b
SanyaP4elkin <104874172+SanyaP4elkin@users.noreply.github.com>
2023-04-13 14:50:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,13 @@ import numpy as np
 chat_id = 1126746074 # Ваш chat ID, не меняйте название переменной
 
 def solution(x) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    if np.mean(x) > 300:
+    
+    m_ar = []
+    for i in range(1000):
+        m = np.random.randint(0, 2, size = len(x))
+        m_ar.append(np.mean(x[m == 1]))
+    
+    if np.mean(m_ar) > 300:
         return False
     else:
         return True
